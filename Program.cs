85925f54bfa7c57b83213ac066ffc5ffef9a7568
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-01 10:38:51 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,73 @@
-﻿using System;
-namespace DelegateExample;
-public class Program
-{
-    public delegate void MessageDelegate(string message);
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    public class Prog
+namespace NumberOperations
+{
+    class Program
     {
         static void Main(string[] args)
         {
-            MessageDelegate messageDelegate = ShowMessage;
+            int number = 8;
+            Predicate<int> isEvenDelegate = IsEven;
+            Predicate<int> isOddDelegate = IsOdd;
+            Predicate<int> isPrimeDelegate = IsPrime;
+            Predicate<int> isFibonacciDelegate = IsFibonacci;
 
-            messageDelegate("Привіт!");
-            messageDelegate("Це текстове повідомлення.");
+            bool isEven = PerformOperation(number, isEvenDelegate);
+            Console.WriteLine($"{number} є парним: {isEven}");
+
+            bool isOdd = PerformOperation(number, isOddDelegate);
+            Console.WriteLine($"{number} є непарним: {isOdd}");
+
+            bool isPrime = PerformOperation(number, isPrimeDelegate);
+            Console.WriteLine($"{number} є простим числом: {isPrime}");
+
+            bool isFibonacci = PerformOperation(number, isFibonacciDelegate);
+            Console.WriteLine($"{number} є числом Фібоначчі: {isFibonacci}");
 
             Console.ReadKey();
         }
-        static void ShowMessage(string message)
+        static bool PerformOperation(int number, Predicate<int> operation)
         {
-            Console.WriteLine("Повідомлення: " + message);
+            return operation(number);
+        }
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+        static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+        static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        static bool IsFibonacci(int number)
+        {
+            int a = 0;
+            int b = 1;
+
+            while (b < number)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b == number;
         }
     }
 }
