455a2b347869a8bc211aa4f965577e5297de7626
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-01 10:17:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DelegateExample;
+public class Program
+{
+    public delegate void MessageDelegate(string message);
+
+    public class Prog
+    {
+        static void Main(string[] args)
+        {
+            MessageDelegate messageDelegate = ShowMessage;
+
+            messageDelegate("Привіт!");
+            messageDelegate("Це текстове повідомлення.");
+
+            Console.ReadKey();
+        }
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine("Повідомлення: " + message);
+        }
+    }
+}
